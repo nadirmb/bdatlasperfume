fd37cacf110b9e99466b72dd18eb2fe5e8bb2fdf
nadirmb <151919443+nadirmb@users.noreply.github.com>
2025-05-05 19:57:35 +0200

    feat: tabla perfume

--- atlasperfume.sql
+++ atlasperfume.sql
@@ -14,3 +14,12 @@ id_iva INT AUTO_INCREMENT PRIMARY KEY,
 tipo VARCHAR(50) NOT NULL,
 descripcion VARCHAR(255)
 );
+
+-- Tabla Perfume
+CREATE TABLE perfume (
+id_perfume INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+precio DECIMAL(10,2) NOT NULL,
+id_pais INT,
+FOREIGN KEY (id_pais) REFERENCES pais(id_pais)
+);
