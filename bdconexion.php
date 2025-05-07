83032713344fd50931483161ae0963eecd6cd5aa
nadirmb <151919443+nadirmb@users.noreply.github.com>
2025-05-07 15:29:46 +0200

    feat: conexion.php datos

---
+++ bdconexion.php
@@ -0,0 +1,6 @@
+<?php
+// datos de la conexión a la base de datos
+$host = "localhost"; //localhoostt
+$user = "root"; //este es el usuario de la base de datos 
+$password = ""; //contraseña de la base de datos
+$dbname = "protect_bird_db"; // aqui ponemos el nombre de la base de datos
