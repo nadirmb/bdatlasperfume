3901c87604eb8f6b43a8bc47c903ba43102127f5
nadirmb <151919443+nadirmb@users.noreply.github.com>
2025-05-07 16:18:47 +0200

    fix: correcion user

--- bdconexion.php
+++ bdconexion.php
@@ -1,8 +1,8 @@
 <?php
 // datos de la conexión a la base de datos
 $host = "localhost"; //localhoostt
-$user = "root"; //este es el usuario de la base de datos 
-$password = ""; //contraseña de la base de datos
+$user = "atlas"; //este es el usuario de la base de datos 
+$password = "atlas12345"; //contraseña de la base de datos
 $dbname = "atlas_perfume"; // aqui ponemos el nombre de la base de datos
 
 // comprobar que la conexion es correcta
