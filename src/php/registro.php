c68ed045c2a27d464297f32ee010392fd13b428c
nadirmb <nadirelmoussaoui6@gmail.com>
2025-05-12 19:37:03 +0200

    fix: formulario

--- src/php/registro.php
+++ src/php/registro.php
@@ -5,5 +5,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $dni = $_POST["dni"];
 $nombre = $_POST["nombre"];
 $correo = $_POST["correo"];
+$telefono = $_POST["telefono"];
+$direccion = $_POST["direccion"];
+$edad = $_POST["edad"];
+$password = password_hash($_POST["password"], PASSWORD_DEFAULT); // hashearemos la contraseña por seguridad 
+// para nuestros usuario asin nosotors no podemos ver sus contraseñsa
 }
 ?> 
