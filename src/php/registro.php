67edff817fd07728c39376c065728763fd25c8dc
nadirmb <nadirelmoussaoui6@gmail.com>
2025-05-12 19:57:04 +0200

    feat: hacemos el inster

--- src/php/registro.php
+++ src/php/registro.php
@@ -1,5 +1,5 @@
 <?php
-include "../php/bdconexion.php";
+include "../php/bdconexion.php"; // incluimos ya el archivo de conexion de bd.conexion.php para poderhacear el registro
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $dni = $_POST["dni"];
@@ -10,5 +10,11 @@ $direccion = $_POST["direccion"];
 $edad = $_POST["edad"];
 $password = password_hash($_POST["password"], PASSWORD_DEFAULT); // hashearemos la contraseña por seguridad 
 // para nuestros usuario asin nosotors no podemos ver sus contraseñsa
+
+$sql = "INSERT INTO cliente (dni, nombre, correo, telefono, direccion, edad, password)
+VALUES (?, ?, ?, ?, ?, ?, ?)"; // dejamos los espacios reservados para el sql 
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sssssis", $dni, $nombre, $correo, $telefono, $direccion, $edad, $password); //
+// sssssis para deicr de que es cada tipo por ejemplo s de string o i de entero
 }
 ?> 
