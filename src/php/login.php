556a9d4bdd19800301e5dd125c3c645bdc58c335
nadirmb <nadirelmoussaoui6@gmail.com>
2025-05-13 12:20:58 +0200

    faet: consulta

--- src/php/login.php
+++ src/php/login.php
@@ -1,5 +1,18 @@
 <?php
+//inicaamos la sesion para guardar datos del usuario
 session_start();
 include 'bdconexion.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //recuperamos datos enviados desde el formulario
+    $usuario = $_POST['usuario'];
+    $clave = $_POST['clave'];
+
+    //consulta sql asin buscamos el usuario en la base de datos
+    $sql = "SELECT * FROM cliente WHERE correo = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $usuario); // nos protegemos contra inyeqccion sql
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+    }
 ?>
