510b08adac3d7f9ede04516fb5b035c929d35a3b
nadirmb <nadirelmoussaoui6@gmail.com>
2025-05-13 14:13:38 +0200

    fix; contraseña por seguridad

--- src/php/bdconexion.php
+++ src/php/bdconexion.php
@@ -2,7 +2,7 @@
 // datos de la conexión a la base de datos
 $host = "localhost"; //localhoostt
 $user = "usuario"; //este es el usuario de la base de datos 
-$password = "1234"; //contraseña de la base de datos
+$password = "Atlasperfume123@"; //contraseña de la base de datos
 $db = "atlas_perfume"; // aqui ponemos el nombre de la base de datos
 
 $conn = new mysqli($host, $user, $password, $db);
