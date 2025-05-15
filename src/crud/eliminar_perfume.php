e85582686b9b89dba9c3f35deb2d39c2b812f86a
nadirmb <nadirelmoussaoui6@gmail.com>
2025-05-15 11:02:55 +0200

    feat: eliminarr_perfume

---
+++ src/crud/eliminar_perfume.php
@@ -0,0 +1,17 @@
+<?php
+include '../php/bdconexion.php'; // conectar bd
+
+$id = $_GET['id']; // id del perfume
+
+// borrar stock primero
+$stmt1 = $conn->prepare("DELETE FROM stock WHERE id_perfume = ?");
+$stmt1->bind_param("i", $id);
+$stmt1->execute();
+
+// borrar perfume
+$stmt2 = $conn->prepare("DELETE FROM perfume WHERE id_perfume = ?");
+$stmt2->bind_param("i", $id);
+$stmt2->execute();
+
+echo "eliminado<br><a href='/crud/listar_perfumes.php'>volver</a>";
+?>
